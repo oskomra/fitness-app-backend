b133d9cf98614022ba9943dd3f5e158bc00f891c
oskskomra <o.skomra@o2.pl>
2025-08-17 12:37:36 +0200

    initialize project

---
+++ src/main/java/osk/sko/FitnessApp/FitnessAppApplication.java
@@ -0,0 +1,13 @@
+package osk.sko.FitnessApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/osk/sko/FitnessApp/FitnessAppApplicationTests.java
@@ -0,0 +1,13 @@
+package osk.sko.FitnessApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
